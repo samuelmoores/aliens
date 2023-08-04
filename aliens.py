8de6e2306bf95429ffad99f19f79893f5d768001
samuelmoores <samueldmoores@gmail.com>
2023-08-04 12:43:47 -0700

    Automatic player movement implemented

--- aliens.py
+++ aliens.py
@@ -22,6 +22,12 @@ def main():
     screen.blit(image_player, (50, 50))
     pygame.display.flip()
 
+    #set move player values
+    xpos = 50
+    ypos = 50
+    step_x = 1
+    step_y = 1
+
     #main loop
     while running:
         #event handling, gets all events from event queue
@@ -29,6 +35,20 @@ def main():
             #check if event type is quit
             if event.type == pygame.QUIT:
                 running = False
+        
+        #move player
+        if xpos > 1024 - 50 or xpos < 0:
+            step_x = -step_x
+        if ypos > 720 - 50 or ypos < 0:
+            step_y = -step_y
+        xpos += step_x
+        ypos += step_y
+        screen.blit(image_bg, (0,0))
+        screen.blit(image_player, (xpos, ypos))
+        pygame.display.flip()
+
+    
+
 
 if __name__=="__main__":
     main()
